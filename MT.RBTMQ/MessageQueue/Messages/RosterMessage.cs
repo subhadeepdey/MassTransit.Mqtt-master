f7cf88e8539e0f2abb1c66ee19495a7263111f2c
Subhadeep Dey <subhadeepdey@gmail.com>
2020-06-04 12:34:26 +0530

    Updated Message

--- MT.RBTMQ/MessageQueue/Messages/RosterMessage.cs
+++ MT.RBTMQ/MessageQueue/Messages/RosterMessage.cs
@@ -4,29 +4,18 @@ using System.Collections.Generic;
 
 namespace MT.RBTMQ.MessageQueue.Messages
 {
-  public class RosterMessage : IRawJsonArrayMessage<RosterItem>
-  {
-    public IEnumerable<RosterItem> Items { get; set; }
-  }
-
-  public class RosterItem
+    public class RosterMessage : IRawJsonArrayMessage<RosterItem>
     {
-    public DateTimeOffset? Timestamp { get; set; }
-    public string Type { get; set; }
-    public string Mac { get; set; }
-    public int GatewayFree { get; set; }
-    public double GatewayLoad { get; set; }
-    public string BleName { get; set; }
-    public string IbeaconUuid { get; set; }
-    public double IbeaconMajor { get; set; }
-    public double IbeaconMinor { get; set; }
-    public double IbeaconTxPower { get; set; }
-    public double Battery { get; set; }
+        public IEnumerable<RosterItem> Items { get; set; }
+    }
 
-    public string Rssi { get; set; }
-    public string RawData { get; set; }
-    public double Temperature { get; set; }
-    public double Humidity { get; set; }
+    public class RosterItem
+    {
 
-  }
+        public string ProviderId { get; set; }
+        public int Npi { get; set; }
+        public string Name { get; set; }
+        public string Addres { get; set; }
+
+    }
 }
